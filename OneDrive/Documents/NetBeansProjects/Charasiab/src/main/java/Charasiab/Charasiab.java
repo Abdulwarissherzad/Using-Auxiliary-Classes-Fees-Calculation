346ae486dfbf5ca6df673bf00451b354e3771bdd
abdulwarissherzad <sherzadwaris52@gmail.com>
2023-08-06 12:36:28 +0200

    Creating a student registration form in Java using MySQL involves several steps, which include setting up the database, designing the user interface, and managing database interactions.. We have added three functionalities to our student registration database in this GUI. Firstly, we've added a button for adding data. Secondly, there is an edit button for modifying records. Lastly, we've included a button to delete records from the form. (Main Form)

---
+++ OneDrive/Documents/NetBeansProjects/Charasiab/src/main/java/Charasiab/Charasiab.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Charasiab;
+
+/**
+ *
+ * @author Abdul Waris Sherzad
+ */
+public class Charasiab {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        reg now = new reg();
+    }
+    
+}
