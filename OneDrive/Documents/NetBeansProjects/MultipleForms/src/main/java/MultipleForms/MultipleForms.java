db38236047ae50f33917c20a21ca076905376d20
abdulwarissherzad <sherzadwaris52@gmail.com>
2023-08-11 02:30:40 +0200

    Multiple Classes, Multiple Forms

---
+++ OneDrive/Documents/NetBeansProjects/MultipleForms/src/main/java/MultipleForms/MultipleForms.java
@@ -0,0 +1,15 @@
+
+package MultipleForms;
+
+
+public class MultipleForms {
+
+    public static void main(String[] args) { 
+       /*
+        
+        */
+        FirstForm F1 =  new FirstForm("First Form of Application");
+       // SecondForm F2 = new SecondForm("Second Form of Application");
+        
+    }
+}
